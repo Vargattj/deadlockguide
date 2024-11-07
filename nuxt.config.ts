0298d4cc2654468ce8f42bc4110caca5e2c34201
jony <vargattjh@gmail.com>
2024-11-07 13:39:44 -0300

    update nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   modules: [
-    '@nuxt/image',
     '@zadigetvoltaire/nuxt-gtm'
   ],
   gtm:{
